a53535949482169769a1f96372ddf5cb151dbf3a
Liam <liam@scalzulli.com>
2023-01-29 19:25:29 -0500

    [A1] Add initial work on hash table

--- src/A1/app/src/main/java/a1/DisjointSets.java
+++ src/A1/app/src/main/java/a1/DisjointSets.java
@@ -1,76 +1,89 @@
+package a1;
+
 import java.io.*;
 import java.util.*;
 
 public class DisjointSets {
-  private int[] par;
-  private int[] rank;
+  private int[] par, rank;
 
   DisjointSets(int n) {
     if (n > 0) {
       par = new int[n];
       rank = new int[n];
-      for (int i = 0; i < this.par.length; i++) {
-        par[i] = i;
-      }
+      for (int i = 0; i < this.par.length; i++) par[i] = i;
     }
   }
 
   public String toString() {
-    int pari, countsets = 0;
+    int count = 0;
 
     String output = "";
 
-    String[] setstrings = new String[this.par.length];
+    String[] strings = new String[this.par.length];
 
     for (int i = 0; i < this.par.length; i++) {
-      pari = find(i);
-      if (setstrings[pari] == null) {
-        setstrings[pari] = String.valueOf(i);
-        countsets += 1;
-      } else {
-        setstrings[pari] += "," + i;
-      }
+      int curr = find(i);
+
+      if (strings[curr] == null) {
+        strings[curr] = String.valueOf(i);
+        count += 1;
+      } else strings[curr] += "," + i;
     }
 
-    output = countsets + " set(s):\n";
+    output = count + " set(s):\n";
 
-    for (int i = 0; i < this.par.length; i++) {
-      if (setstrings[i] != null) {
-        output += i + " : " + setstrings[i] + "\n";
-      }
-    }
+    for (int i = 0; i < this.par.length; i++)
+      if (strings[i] != null) output += i + " : " + strings[i] + "\n";
 
     return output;
   }
 
   public int find(int i) {
-    return 0;
+    if (par[i] == i) return i;
+    return par[i] = find(par[i]);
   }
 
   public int union(int i, int j) {
-    return 0;
+    if (find(i) == find(j)) return 0;
+
+    int a = find(i), b = find(j);
+
+    return 1;
   }
 
   public static void main(String[] args) {
     DisjointSets myset = new DisjointSets(6);
+
     System.out.println(myset);
     System.out.println("-> Union 2 and 3");
+
     myset.union(2, 3);
+
     System.out.println(myset);
     System.out.println("-> Union 2 and 3");
+
     myset.union(2, 3);
+
     System.out.println(myset);
     System.out.println("-> Union 2 and 1");
+
     myset.union(2, 1);
+
     System.out.println(myset);
     System.out.println("-> Union 4 and 5");
+
     myset.union(4, 5);
+
     System.out.println(myset);
     System.out.println("-> Union 3 and 1");
+
     myset.union(3, 1);
+
     System.out.println(myset);
     System.out.println("-> Union 2 and 4");
+
     myset.union(2, 4);
+
     System.out.println(myset);
   }
 }
--- src/A1/app/src/main/java/a1/OpenAddressing.java
+++ src/A1/app/src/main/java/a1/OpenAddressing.java
@@ -1,69 +1,67 @@
+package a1;
+
 import java.io.*;
 import java.util.*;
 
 public class OpenAddressing {
-  public int m; // number of SLOTS AVAILABLE
-  public int A; // the default random number
-  int w;
-  int r;
+  public int m, A;
+  int w, r;
   public int[] Table;
 
   protected OpenAddressing(int w, int seed, int A) {
     this.w = w;
-    this.r = (int) (w - 1) / 2 + 1;
+    this.r = (w - 1) / 2 + 1;
     this.m = power2(r);
 
-    if (A == -1) {
-      this.A = generateRandom((int) power2(w - 1), (int) power2(w), seed);
-    } else {
-      this.A = A;
-    }
+    if (A == -1) this.A = generateRandom(power2(w - 1), power2(w), seed);
+    else this.A = A;
 
     this.Table = new int[m];
 
-    for (int i = 0; i < m; i++) {
-      Table[i] = -1;
-    }
+    for (int i = 0; i < m; i++) Table[i] = -1;
   }
 
-  /** Calculate 2^w */
   public static int power2(int w) {
     return (int) Math.pow(2, w);
   }
 
   public static int generateRandom(int min, int max, int seed) {
     Random generator = new Random();
-    if (seed >= 0) {
-      generator.setSeed(seed);
-    }
-    int i = generator.nextInt(max - min - 1);
-    return i + min + 1;
+    if (seed >= 0) generator.setSeed(seed);
+    return generator.nextInt(max - min - 1) + min + 1;
   }
 
-  /** Implements the hash function g(k) */
+  private int probe(int key) {
+    return ((A * key) % power2(w) >> (w - r));
+  }
+
   public int probe(int key, int i) {
-    // TODO: implement this function and change the return statement.
-    return -1;
+    return (probe(key) + i) % power2(r);
   }
 
-  /** Inserts key k into hash table. Returns the number of collisions encountered */
   public int insertKey(int key) {
-    // TODO : implement this and change the return statement.
-    return -1;
+    int probe = 0, curr = probe(key, probe++);
+    while (Table[curr] != -1) curr = probe(key, probe++);
+    Table[curr] = key;
+    return probe;
   }
 
-  /** Sequentially inserts a list of keys into the HashTable. Outputs total number of collisions */
   public int insertKeyArray(int[] keyArray) {
     int collision = 0;
-    for (int key : keyArray) {
-      collision += insertKey(key);
-    }
+    for (int key : keyArray) collision += insertKey(key);
     return collision;
   }
 
-  /** Inserts key k into hash table. Returns the number of collisions encountered */
   public int removeKey(int key) {
-    // TODO: implement this and change the return statement
-    return -1;
+    int probe = 0, curr = probe(key, probe++), first = curr;
+
+    while (Table[curr] != key) {
+      curr = probe(key, probe++);
+      if (curr == first) return 0;
+    }
+
+    Table[curr] = -1;
+
+    return probe;
   }
 }
