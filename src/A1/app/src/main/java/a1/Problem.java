11c0a06039585c61b7cef8de33c1af325a28275d
Liam <liam@scalzulli.com>
2023-01-30 15:30:20 -0500

    [A1] Complete the leetcode problem

--- src/A1/app/src/main/java/a1/Problem.java
+++ src/A1/app/src/main/java/a1/Problem.java
@@ -1,7 +1,19 @@
+package a1;
+
 import java.util.*;
 
 public class Problem {
   public static int elements(int[] sizes) {
-    return 0;
+    HashMap<Integer, Integer> store = new HashMap<Integer, Integer>();
+
+    int ans = 0, j = 0;
+
+    for (int i = 0; i < sizes.length; ++i) {
+      if (store.containsKey(sizes[i])) j = Math.max(j, store.get(sizes[i]) + 1);
+      ans = Math.max(ans, i - j + 1);
+      store.put(sizes[i], i);
+    }
+
+    return ans;
   }
 }
