fa044d1f0cceffc7b0ee7be02377847c31a4606e
Liam <liam@scalzulli.com>
2023-03-06 11:13:04 -0500

    [A2] Add work for Q1 (18/20)

--- src/A2/app/src/main/java/a2/A2_Q1.java
+++ src/A2/app/src/main/java/a2/A2_Q1.java
@@ -2,8 +2,73 @@ package a2;
 
 import java.util.*;
 
+class Move {
+  public int sX, sY, mX, mY, eX, eY;
+
+  public Move(int sX, int sY, int eX, int eY) {
+    this.sX = sX;
+    this.sY = sY;
+    this.mX = (sX + eX) / 2;
+    this.mY = (sY + eY) / 2;
+    this.eX = eX;
+    this.eY = eY;
+  }
+}
+
 public class A2_Q1 {
+  static ArrayList<Move> allMoves(int[][] board) {
+    int row = board.length, col = board[0].length;
+
+    int[][] directions = {{0, 2}, {0, -2}, {2, 0}, {-2, 0}, {2, 2}, {-2, -2}};
+
+    ArrayList<Move> moves = new ArrayList<>();
+
+    for (int i = 0; i < row; ++i) {
+      for (int j = 0; j < col; ++j) {
+        if (board[i][j] <= 0) continue;
+        for (int[] direction : directions) {
+          int r = i + direction[0], c = j + direction[1];
+          if (r >= 0
+              && r < row
+              && c >= 0
+              && c < col
+              && board[r][c] == 0
+              && board[(i + r) / 2][(j + c) / 2] > 0) moves.add(new Move(i, j, r, c));
+        }
+      }
+    }
+
+    return moves;
+  }
+
+  static int helper(int[][] board, boolean turn, int sa, int sb) {
+    ArrayList<Move> moves = allMoves(board);
+
+    if (moves.isEmpty()) return sa - sb;
+
+    int ans = turn ? 0 : Integer.MAX_VALUE;
+
+    for (Move move : moves) {
+      int score = board[move.sX][move.sY] * board[move.mX][move.mY],
+          start = board[move.sX][move.sY],
+          mid = board[move.mX][move.mY];
+
+      board[move.sX][move.sY] = 0;
+      board[move.mX][move.mY] = 0;
+      board[move.eX][move.eY] = start;
+
+      if (turn) ans = Math.max(ans, helper(board, !turn, sa + score, sb));
+      else ans = Math.min(ans, helper(board, !turn, sa, sb + score));
+
+      board[move.sX][move.sY] = start;
+      board[move.mX][move.mY] = mid;
+      board[move.eX][move.eY] = 0;
+    }
+
+    return ans;
+  }
+
   public static int game_recursion(int[][] board) {
-    return 0;
+    return helper(board, true, 0, 0);
   }
 }
--- src/A2/app/src/main/java/a2/App.java
+++ src/A2/app/src/main/java/a2/App.java
@@ -1,7 +1,66 @@
 package a2;
 
+/*
+ * 3
+ * 1 0
+ * 1 0 8
+ * 5 6 3 4
+ * 9 3 2 1 9
+ */
+
 public class App {
   public static void main(String[] args) {
-    System.out.println("Hello, world!");
+    System.out.println(
+        A2_Q1.game_recursion(
+            new int[][] {
+              {0, -1, -1},
+              {1, 2, -1},
+              {3, 4, 5},
+            }));
+    System.out.println(
+        A2_Q1.game_recursion(
+            new int[][] {
+              {3, -1, -1, -1, -1},
+              {1, 6, -1, -1, -1},
+              {1, 7, 8, -1, -1},
+              {5, 0, 3, 4, -1},
+              {9, 3, 2, 1, 9}
+            }));
+    System.out.println(
+        A2_Q1.game_recursion(
+            new int[][] {
+              {0, -1, -1, -1, -1},
+              {1, 1, -1, -1, -1},
+              {1, 1, 1, -1, -1},
+              {1, 1, 1, 1, -1},
+              {1, 1, 1, 1, 1}
+            }));
+    System.out.println(
+        A2_Q1.game_recursion(
+            new int[][] {
+              {1, -1, -1, -1, -1},
+              {1, 1, -1, -1, -1},
+              {1, 1, 1, -1, -1},
+              {1, 1, 1, 1, -1},
+              {1, 1, 0, 100, 1}
+            }));
+    System.out.println(
+        A2_Q1.game_recursion(
+            new int[][] {
+              {55, -1, -1, -1, -1},
+              {99, 24, -1, -1, -1},
+              {77, 93, 19, -1, -1},
+              {27, 26, 5, 53, -1},
+              {9, 90, 48, 44, 0}
+            }));
+    System.out.println(
+        A2_Q1.game_recursion(
+            new int[][] {
+              {56, -1, -1, -1, -1},
+              {12, 56, -1, -1, -1},
+              {56, 16, 51, -1, -1},
+              {34, 56, 56, 0, -1},
+              {56, 22, 56, 43, 56}
+            }));
   }
 }
