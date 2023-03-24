2b089589e9566c1da56a1f5f5e65c084effa6a52
Liam <liam@scalzulli.com>
2023-03-24 12:15:54 -0400

    [A3] Don't use an inner class in Q2

--- src/A3/app/src/main/java/a3/A3Q2.java
+++ src/A3/app/src/main/java/a3/A3Q2.java
@@ -3,21 +3,12 @@ package a3;
 import java.util.*;
 
 public class A3Q2 {
-  static class Node {
-    public int piece, quantity;
-
-    public Node(int piece, int quantity) {
-      this.piece = piece;
-      this.quantity = quantity;
-    }
-  }
-
-  static List<Integer> sort(ArrayList<ArrayList<Node>> adj) {
+  static List<Integer> sort(ArrayList<ArrayList<int[]>> adj) {
     int n = adj.size();
 
     int[] deg = new int[n];
 
-    for (List<Node> nodes : adj) for (Node node : nodes) deg[node.piece]++;
+    for (List<int[]> nodes : adj) for (int[] node : nodes) deg[node[0]]++;
 
     Queue<Integer> queue = new LinkedList<>();
 
@@ -28,7 +19,7 @@ public class A3Q2 {
     while (!queue.isEmpty()) {
       int v = queue.poll();
       ret.add(v);
-      for (Node node : adj.get(v)) if (--deg[node.piece] == 0) queue.add(node.piece);
+      for (int[] node : adj.get(v)) if (--deg[node[0]] == 0) queue.add(node[0]);
     }
 
     return ret;
@@ -37,14 +28,14 @@ public class A3Q2 {
   public static long[] num_pieces(long[] pieces, int[][] instructions) {
     int n = pieces.length;
 
-    ArrayList<ArrayList<Node>> adj = new ArrayList<>();
+    ArrayList<ArrayList<int[]>> adj = new ArrayList<>();
 
     for (int i = 0; i < n; ++i) adj.add(new ArrayList<>());
 
-    for (int[] i : instructions) adj.get(i[1]).add(new Node(i[0], i[2]));
+    for (int[] i : instructions) adj.get(i[1]).add(new int[] {i[0], i[2]});
 
     for (int i : sort(adj))
-      for (Node node : adj.get(i)) pieces[node.piece] += pieces[i] * node.quantity;
+      for (int[] node : adj.get(i)) pieces[node[0]] += pieces[i] * node[1];
 
     return pieces;
   }
