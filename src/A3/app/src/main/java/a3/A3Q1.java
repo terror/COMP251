c1b1d5dbeaf2cb4edf0a578fadc18c8245ec7dfb
Liam <liam@scalzulli.com>
2023-03-24 12:12:18 -0400

    [A3] Don't use inner class in Q1

--- src/A3/app/src/main/java/a3/A3Q1.java
+++ src/A3/app/src/main/java/a3/A3Q1.java
@@ -3,22 +3,6 @@ package a3;
 import java.util.*;
 
 public class A3Q1 {
-  static class Node {
-    public int x, y, count, time;
-
-    public Node(int x, int y) {
-      this.x = x;
-      this.y = y;
-    }
-
-    public Node(int x, int y, int count, int time) {
-      this.x = x;
-      this.y = y;
-      this.count = count;
-      this.time = time;
-    }
-  }
-
   static boolean validMove(
       String[][] board, boolean[][] vis, int row, int col, int x, int y, String direction) {
     if (x < 0 || x >= row || y < 0 || y >= col || vis[x][y]) return false;
@@ -35,26 +19,26 @@ public class A3Q1 {
     return true;
   }
 
-  static Node getOffset(String dir) {
+  static int[] getOffset(String dir) {
     switch (dir) {
       case "U":
-        return new Node(-1, 0);
+        return new int[] {-1, 0};
       case "D":
-        return new Node(1, 0);
+        return new int[] {1, 0};
       case "L":
-        return new Node(0, -1);
+        return new int[] {0, -1};
       case "R":
-        return new Node(0, 1);
+        return new int[] {0, 1};
       default:
-        return null;
+        return new int[] {};
     }
   }
 
-  static Node getStartingPosition(int time, String[][] board) {
+  static int[] getStartingPosition(int time, String[][] board) {
     for (int i = 0; i < board.length; ++i)
       for (int j = 0; j < board[0].length; ++j)
-        if (board[i][j].equals("S")) return new Node(i, j, 0, time);
-    return null;
+        if (board[i][j].equals("S")) return new int[] {i, j, 0, time};
+    return new int[] {};
   }
 
   public static int find_exit(int time, String[][] board) {
@@ -62,29 +46,29 @@ public class A3Q1 {
 
     boolean[][] vis = new boolean[row][col];
 
-    Queue<Node> q = new LinkedList<>();
+    Queue<int[]> q = new LinkedList<>();
 
-    Node start = getStartingPosition(time, board);
+    int[] start = getStartingPosition(time, board);
     q.add(start);
-    vis[start.x][start.y] = true;
+    vis[start[0]][start[1]] = true;
 
     String[] directions = new String[] {"U", "D", "L", "R"};
 
     int ans = Integer.MAX_VALUE;
 
     while (!q.isEmpty()) {
-      Node curr = q.remove();
+      int[] curr = q.remove();
 
-      if (curr.x == row - 1 || curr.x == 0 || curr.y == col - 1 || curr.y == 0)
-        ans = Math.min(ans, curr.count);
+      if (curr[0] == row - 1 || curr[0] == 0 || curr[1] == col - 1 || curr[1] == 0)
+        ans = Math.min(ans, curr[2]);
 
       for (int i = 0; i < 4; ++i) {
-        Node offset = getOffset(directions[i]);
+        int[] offset = getOffset(directions[i]);
 
-        int x = curr.x + offset.x, y = curr.y + offset.y;
+        int x = curr[0] + offset[0], y = curr[1] + offset[1];
 
-        if (validMove(board, vis, row, col, x, y, directions[i]) && curr.time > 0) {
-          q.add(new Node(x, y, curr.count + 1, curr.time - 1));
+        if (validMove(board, vis, row, col, x, y, directions[i]) && curr[3] > 0) {
+          q.add(new int[] {x, y, curr[2] + 1, curr[3] - 1});
           vis[x][y] = true;
         }
       }
